ad9336b6dda9daf1b579386fa8a0791988964247
bimows <38582433+bimows@users.noreply.github.com>
2018-05-26 05:11:05 +0700

    Update latihanPYTHON.py
    Nama: Muhammad Bimo
    NIM:231.14.0091

--- latihanPYTHON.py
+++ latihanPYTHON.py
@@ -1,17 +1,37 @@
+#Bagaimana untuk menampilkan variabel current_exercise dan todays_date? 
 greeting_message = "Welcome to Codecademy!"
 current_excercise = 5
 todays_date = "March 31, 2023"
-print greeting_message
-%Bagaimana untuk menampilkan variabel current_exercise dan todays_date? 
+print "Tampilanya seperti ini: {}, {}".format(current_excercise, todays_date)
+print "Atau Seperti ini:"
+print current_excercise
+print todays_date
+print " "
 
+# Bagaimana untuk menampilkan variabel mirthful_addition, amazing_subtraction,trippy_multiplication, dan sassy_combinations?
 mirthful_addition = 12381 + 91817
 amazing_subtraction = 981 - 312
 trippy_multiplication = 38 * 902
 happy_division = 540 / 45
 sassy_combinations = 129 * 1345 + 120 / 6 - 12
-print happy_division
-%Bagaimana untuk menampilkan variabel mirthful_addition, amazing_subtraction,trippy_multiplication, dan sassy_combinations? 
+print "Tampilanya seperti ini: {}, {}, {}, {}".format(mirthful_addition, amazing_subtraction, trippy_multiplication, sassy_combinations)
+print "Atau Seperti ini:"
+print mirthful_addition
+print amazing_subtraction
+print trippy_multiplication
+print sassy_combinations
+print " "
 
+# Menulis Komentar
 city_name = "St. Potatosburg"
 city_pop = 340000
-%Bagaimana cara membuat komen?
+
+# Ini coba nulis komentar
+print city_name
+print city_pop
+
+"""
+Bimo Sedang Mengomentari
+tapi kurang panjang
+dan bingung mau nulis apa.
+"""
