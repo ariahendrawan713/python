e2b9ceefbd2ab400e6bd52dcd3d8f73e4fffbcf8
manitajay <54741067+manitajay@users.noreply.github.com>
2020-10-02 21:46:17 +0530

    update 1.py

--- 1.py
+++ 1.py
@@ -2,14 +2,14 @@ my_name = 'Zed A. Shaw'
 my_age = 35 # not a lie
 my_height = 74 # inches
 my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+my_eyes = 'Blue' # eye colour
+my_teeth = 'White' #teeth colout
+my_hair = 'Brown' #hair colour
 
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
-print("Actually that's not too heavy.")
+print(f"He's {my_weight} is not heavy.")
+
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
