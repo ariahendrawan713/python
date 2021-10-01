86254cafe373281706938eaedc4dce7951175978
kaluwedana <kaluwedana97@gmail.com>
2021-10-01 15:34:07 +0530

    update 1.py

--- 1.py
+++ 1.py
@@ -11,7 +11,7 @@ print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
 print("Actually that's not too heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"His teeth are usually {my_teeth} depending on the coffee..")
 
 # this line is tricky, try to get it exactly right
 total = my_age + my_height + my_weight
